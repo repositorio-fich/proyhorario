c182b1c579bd9642820e74af92d6011b9a5c248e
alfreding0 <alfredogonzales953@gmail.com>
2021-01-21 19:53:33 -0400

    probando en main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,15 @@ package main;
 /** @author alfreding0 */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Haciendo cambios sin saber!!!");
+        System.out.println("linea 1");
+        System.out.println("linea 1");
+        System.out.println("linea 1");
+        System.out.println("linea 1");
         
+        System.out.println("linea 1");
+        System.out.println("linea 1");
+        System.out.println("linea 1");
     }
 
 }
